f9e5d40c350c3b2bb31841dfb3cea8099ad7199f
VladimirNikel <vova_niemisto@mail.ru>
2020-10-14 22:47:29 +0300

    Добавлен многострадальный GraphQL в сервис А

--- a_service.py
+++ a_service.py
@@ -6,7 +6,7 @@
 #Если код не валиден, то возвращается ошибка, иначе, телефону проставляется признак верификации и возвращается индикация успеха (любая на выбор).
 
 #Для упрощения данные в обоих сервисах вынесены в код (БД использовать не нужно).
-#Взаимодействие между сервисами должно осуществляться по PRC с использованием gRPC или Thrift.
+#Взаимодействие между сервисами должно осуществляться по RPC с использованием gRPC или Thrift.
 #Сервис А принимает запросы GraphQL
 
 from typing import Optional
@@ -15,12 +15,57 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 import uvicorn
 
-#необходимо, чтобы работать с json'ом
 import json
-
-#необходимо для работы с переменными окружения
-import os
 import requests
+import graphene
+
+telefon_list=[]
+def check_pin(tel:str, pin:str, true_pin:str):
+	#функция проверки кода
+	if pin.isdigit() and pin == true_pin:
+		telefon_list.append([tel, pin, 'False'])
+		print(telefon_list)
+		return json.dumps({"tel": tel,"pin": pin, "answer": "Введенный код верен. Радуйтесь жизни."})
+	else:
+		return json.dumps({"tel": tel,"pin": pin, "error": "Бьемся в истерике: введенный код неправильный."})
+
+
+class Query(graphene.ObjectType):
+	telefonNumber = graphene.String()
+	pin = graphene.String(args={"telefonNumber": graphene.String(), "pin": graphene.String()})
+	status = graphene.String(args={"telefonNumber": graphene.String(), "pin": graphene.String()})
+
+	def resolve_status(self, info, **args):
+		#опрос временной таблицы, которая хранится только в этом сервисе
+		for i in telefon_list:
+			temp_mass=[]
+			temp_mass.extend(i)
+			if args["telefonNumber"] in temp_mass:
+				if args["pin"]==temp_mass[2]:
+					return temp_mass[2]
+				else:
+					return "pin neveren"
+		
+		#если телефон не найден во временной таблице
+		context_query='http://localhost:8002/check_tel'		#запрос к серверу Б
+		request = requests.get(context_query,params={'tel': args["telefonNumber"] }).text
+		if request == "true":	#телефон найден в базе сервиса Б
+			if args["pin"].isdigit():
+				telefon_list.append([args["telefonNumber"], args["pin"], 'Confirmed'])		#занесение во временную таблицу данных, что код валиден
+			else: 				#код не валиден
+				telefon_list.append([args["telefonNumber"], args["pin"], 'Unconfirmed'])	#занесение во временную таблицу данных, что код не валиден
+				#возвращение какой-то ошибки должно быть
+				return "pin nevaliden"
+			for i in telefon_list:
+				temp_mass=[]
+				temp_mass.extend(i)	
+				if args["telefonNumber"] in temp_mass:
+					return temp_mass[2]
+		else:
+			#вовзращение еще какой-то ошибки, что телефон невалиден
+			return "not good - telefona net"
+
+schema = graphene.Schema(query=Query)
 
 @app.get("/")
 def print_web():
@@ -30,14 +75,14 @@ def print_web():
 	<title>Сервис верификации телефонных номеров</title>
 	<style>
 		button.knopka {
-		color: #fff; 
-		background: #FFA500; 
-		padding: 5px; 
-		border-radius: 5px;
-		border: 2px solid #FF8247;
+			color: #fff; 
+			background: #FFA500; 
+			padding: 5px; 
+			border-radius: 5px;
+			border: 2px solid #FF8247;
 		} 
 		button.knopka:hover { 
-		background: #FF6347; 
+			background: #FF6347; 
 		}
 	</style>
 	</head>
@@ -45,14 +90,16 @@ def print_web():
 		<h1>Сервис верификации телефонных номеров</h1>
 		<table>
 		<tr>
-			<td>
-				<p>Провести верификацию телефонного номера и кода к нему</p>
-				<form action="/verif" method="GET" name="form1">
-					<input name="tel" type="text" />
-					<input name="pin" type="text" />
+			<form action="/verif" method="GET" name="form1">
+				<td>
+					<p>Провести верификацию телефонного номера и кода к нему</p>
+					<p>Введите ваш запрос в форме GraphQL</p>
+					<textarea name="data" cols="40" rows="10"></textarea>
+				</td>
+				<td>
 					<button class="knopka">Провести проверку</button>
-				</form>
-			</td>
+				</td>
+			</form>
 		</tr>
 		</table>
 	</body>
@@ -61,22 +108,22 @@ def print_web():
 	return HTMLResponse(content=html_content, status_code=200)
 
 @app.get("/verif")
-def verif(tel: str, pin: str):
-	context_query='http://localhost:8002/check_tel'
-	request = requests.get(context_query,params={'tel': str(tel)}).text
-	print("type request: ",type(request),"\trequest: ",request)
-	if request == "true":
-		#телефон найден в базе
-
-		#функция проверки кода
-		if pin == "1234":
-			return json.dumps({"tel": tel,"pin": pin, "answer": "Введенный код верен. Радуйтесь жизни."})
-		else:
-			return json.dumps({"tel": tel,"pin": pin, "error": "Бьемся в истерике: введенный код неправильный."})
-
-	else:
-		return json.dumps({"tel": tel,"pin": pin, "error": "Указанный телефон не существует"})
-
+def verif(data: str):
+	print("data: ", data)
+	result = schema.execute(data)
+	return str(result)
+#	for i in telefon_list:
+#		temp_mass=[]
+#		temp_mass.extend(i)
+#		if tel in temp_mass:
+#			return check_pin(tel, pin, temp_mass[1])
+	#если телефон не найден во временной таблице
+#	context_query='http://localhost:8002/check_tel'
+#	request = requests.get(context_query,params={'tel': str(tel)}).text
+#	if request == "true":	#телефон найден в базе
+#		return check_pin(tel, pin, pin)
+#	else:
+#		return json.dumps({"tel": tel,"pin": pin, "error": "Указанный телефон не существует"})
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
