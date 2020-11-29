e5d3fb8f34146ab2547016873cb699e78ca133de
VladimirNikel <vova_niemisto@mail.ru>
2020-11-29 23:29:58 +0300

    полная подготовка ко второму сервису

--- README.md
+++ README.md
@@ -16,18 +16,39 @@
 ## Инструкция по установке:
 1. Скачать/стянуть репозиторий
 1. Перейти в папку репозитория
+1. Из терминала выполнить команды:
+	```bash
+	python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. verification.proto
+	protoc -I . --cpp_out=. ./verification.proto
+	```
 1. Запустить *сервис А* командой `python3 a_service.py`
 1. Запустить *сервис Б* командой ` `
 1. Выполняем запрос к сервису А
-  * Либо переходим в браузер [сюда](http://127.0.0.1:8000)
+  * Либо переходим в браузер [сюда](http://127.0.0.1:8000) и ввести `{status(phone: "<номер телефона>", pin:"<пин код>")}` в единственное поле
   * Либо выполняем команду из терминала 
   	* `curl "http://127.0.0.1:8000/verif?data=%7B+status+%28telefonNumber%3A+%22<номер телефона>%22%2C+pin%3A+%22<код>%22%29%7D"`
 
 
 
+
 ## Инструментарий:
 - GIT (устанавливается командой `sudo apt install git -y`)
-- GCC (устанавливается командой `sudo apt install build-essential`) [дополнительная инструкция](https://losst.ru/ustanovka-gcc-v-ubuntu-16-04)
+- protobuf (устанавливается командой `sudo apt  install protobuf-compiler`)
+- GCC и доп.модули (устанавливается командой `sudo apt install -y build-essential autoconf libtool pkg-config`)
+- grpc устанавливается выполнением команд:
+	```
+	git clone --recurse-submodules -b v1.33.2 https://github.com/grpc/grpc
+	cd grpc
+	mkdir -p cmake/build
+	pushd cmake/build
+	cmake -DgRPC_INSTALL=ON \
+		-DgRPC_BUILD_TESTS=OFF \
+		-DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
+		../..
+	make -j
+	make install
+	popd
+	```
 - Установленные модули:
 	+ FastAPI `sudo pip3 install fastapi`
 	+ Unicorn `sudo pip3 install uvicorn`
--- a_service.py
+++ a_service.py
@@ -10,7 +10,6 @@
 #Сервис А принимает запросы GraphQL
 import grpc
 
-from typing import Optional
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 app = FastAPI()
@@ -21,47 +20,51 @@ import requests
 import graphene
 
 telefon_list=[]
-def check_pin(tel:str, pin:str, true_pin:str):
-	#функция проверки кода
-	if pin.isdigit() and pin == true_pin:
-		telefon_list.append([tel, pin, 'False'])
-		print(telefon_list)
-		return json.dumps({"tel": tel,"pin": pin, "answer": "Введенный код верен. Радуйтесь жизни."})
-	else:
-		return json.dumps({"tel": tel,"pin": pin, "error": "Бьемся в истерике: введенный код неправильный."})
 
+def check_valid_code(phone: str, pin: str):
+	if pin.isdigit() and (pin >= 0 and pin <= 9999):
+		telefon_list.append([phone, pin, 'Confirmed'])		#занесение во временную таблицу данных, что код валиден
+	else:
+		telefon_list.append([phone, pin, 'Unconfirmed'])	#занесение во временную таблицу данных, что код не валиден
+		return "pin nevaliden"
+	for i in telefon_list:
+		temp_mass=[]
+		temp_mass.extend(i)	
+		if phone in temp_mass:
+			return temp_mass[2]
 
 class Query(graphene.ObjectType):
-	telefonNumber = graphene.String()
-	pin = graphene.String(args={"telefonNumber": graphene.String(), "pin": graphene.String()})
-	status = graphene.String(args={"telefonNumber": graphene.String(), "pin": graphene.String()})
+	status = graphene.String(args={"phone": graphene.String(), "pin": graphene.String()})
 
 	def resolve_status(self, info, **args):
-		#опрос временной таблицы, которая хранится только в этом сервисе
+		phone = args["phone"]
+		pin = args["pin"]
+
 		for i in telefon_list:
 			temp_mass=[]
 			temp_mass.extend(i)
-			if args["telefonNumber"] in temp_mass:
-				if args["pin"]==temp_mass[2]:
+			if phone in temp_mass:
+				if pin == temp_mass[2]:
 					return temp_mass[2]
 				else:
 					return "pin neveren"
 		
 		#если телефон не найден во временной таблице
-		context_query='http://localhost:8002/check_tel'		#запрос к серверу Б
-		request = requests.get(context_query,params={'tel': args["telefonNumber"] }).text
-		if request == "true":	#телефон найден в базе сервиса Б
-			if args["pin"].isdigit():
-				telefon_list.append([args["telefonNumber"], args["pin"], 'Confirmed'])		#занесение во временную таблицу данных, что код валиден
-			else: 				#код не валиден
-				telefon_list.append([args["telefonNumber"], args["pin"], 'Unconfirmed'])	#занесение во временную таблицу данных, что код не валиден
-				#возвращение какой-то ошибки должно быть
-				return "pin nevaliden"
-			for i in telefon_list:
-				temp_mass=[]
-				temp_mass.extend(i)	
-				if args["telefonNumber"] in temp_mass:
-					return temp_mass[2]
+		channel 	= grpc.insecure_channel('localhost:8002')							#open a gRPS channel
+		stub 		= verification_pb2_grps.VerificationStub(channel)					#create a stub (client)
+		number 		= verification_pb2.PhonePinRequest(phone=int(phone), pin=int(pin))	#create a valid request message
+		response 	= stub.CheckValidTelefon(number)									#обращение к сервису B
+		print("response.status\t", response.status)
+		
+
+		#context_query='http://localhost:8002/check_tel'
+		#request = requests.get(context_query,params={'phone': str(args["phone"])}).text
+		#print("type request: ",type(request),"\trequest: ",request)
+		#if request == "true":
+		
+
+		if response.status == "True":	#телефон найден в базе сервиса Б
+			return check_valid_code(phone, pin)
 		else:
 			#вовзращение еще какой-то ошибки, что телефон невалиден
 			return "not good - telefona net"
@@ -70,61 +73,15 @@ schema = graphene.Schema(query=Query)
 
 @app.get("/")
 def print_web():
-	html_content = """
-		<html>
-	<head>
-	<title>Сервис верификации телефонных номеров</title>
-	<style>
-		button.knopka {
-			color: #fff; 
-			background: #FFA500; 
-			padding: 5px; 
-			border-radius: 5px;
-			border: 2px solid #FF8247;
-		} 
-		button.knopka:hover { 
-			background: #FF6347; 
-		}
-	</style>
-	</head>
-	<body>
-		<h1>Сервис верификации телефонных номеров</h1>
-		<table>
-		<tr>
-			<form action="/verif" method="GET" name="form1">
-				<td>
-					<p>Провести верификацию телефонного номера и кода к нему</p>
-					<p>Введите ваш запрос в форме GraphQL</p>
-					<textarea name="data" cols="40" rows="10"></textarea>
-				</td>
-				<td>
-					<button class="knopka">Провести проверку</button>
-				</td>
-			</form>
-		</tr>
-		</table>
-	</body>
-	</html>
-	"""
+	html_content = open('./index.html', 'r').read()
 	return HTMLResponse(content=html_content, status_code=200)
 
 @app.get("/verif")
 def verif(data: str):
-	#print("data: ", data)					# { status (telefonNumber: "1545784848818", pin: "3333")}
+	print("data\t",data)
 	result = schema.execute(data)
+	print(result)
 	return str(result)
-#	for i in telefon_list:
-#		temp_mass=[]
-#		temp_mass.extend(i)
-#		if tel in temp_mass:
-#			return check_pin(tel, pin, temp_mass[1])
-	#если телефон не найден во временной таблице
-#	context_query='http://localhost:8002/check_tel'
-#	request = requests.get(context_query,params={'tel': str(tel)}).text
-#	if request == "true":	#телефон найден в базе
-#		return check_pin(tel, pin, pin)
-#	else:
-#		return json.dumps({"tel": tel,"pin": pin, "error": "Указанный телефон не существует"})
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
